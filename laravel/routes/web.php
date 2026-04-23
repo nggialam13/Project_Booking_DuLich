8e806aace6b3180618cfb39573087e92bf7128db
Dangfind <vuhoanginhdang@gmail.com>
2026-04-23 15:38:06 +0700

    Revert "crud list user"
    This reverts commit 50b15c85843e9a22de0587780bc21a66b8c7033f.

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TourController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Admin Tours List
-Route::get('/admin/tours', [TourController::class, 'index'])->name('tours.index');
-
