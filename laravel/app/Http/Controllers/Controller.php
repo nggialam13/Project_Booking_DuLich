c306c9e961cb1f07373c89a08455708e06e641d8
nggialam13 <gialam130704@gmail.com>
2026-04-24 09:43:11 +0700

    update: add master layout (navbar + footer + alert)

--- laravel/app/Http/Controllers/Controller.php
+++ laravel/app/Http/Controllers/Controller.php
@@ -5,4 +5,9 @@ namespace App\Http\Controllers;
 abstract class Controller
 {
     //
+    public function index()
+{
+    $tours = Tour::all();
+    return view('tours.index', compact('tours'));
+}
 }
--- laravel/resources/views/bookings/index.blade.php
+++ laravel/resources/views/bookings/index.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.master')
+
+@section('content')
 <!DOCTYPE html>
 <html lang="vi">
 <head>
@@ -83,3 +86,4 @@
 
 </body>
 </html>
+@endsection
