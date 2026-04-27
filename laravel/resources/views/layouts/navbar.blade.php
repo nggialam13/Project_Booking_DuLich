e2118d0b93f621bb5c45388f5c30174947dde3a8
pdHanh <pdhanh.12a3.2020@gmail.com>
2026-04-27 18:48:11 +0700

    update nav

--- laravel/resources/views/layouts/navbar.blade.php
+++ laravel/resources/views/layouts/navbar.blade.php
@@ -6,10 +6,31 @@
             <div>
                 <a href="/tours" class="btn btn-light me-2">Tours</a>
                 <a href="/bookings" class="btn btn-outline-light">Booking</a>
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <button type="submit" class="btn btn-outline-light">Đăng xuất</button>
-                </form>
+                <ul class="navbar-nav ms-auto">
+                    @guest
+                        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Đăng nhập</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Đăng ký</a></li>
+                    @else
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                {{ Auth::user()->name }}
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="{{ route('profile') }}">Hồ sơ của tôi</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li>
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+                                        <button type="submit" class="dropdown-item">Đăng xuất</button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endguest
+                </ul>
             </div>
         </div>
     </nav>
